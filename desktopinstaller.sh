8587ac9c6ebfb820b2bc19e9ee72c578d842df2a
Orian Zinger <zinger777@gmail.com>
2020-04-13 15:56:22 +0300

    Splitting installers to desktop and RPi

---
+++ desktopinstaller.sh
@@ -0,0 +1,8 @@
+sudo mkdir /etc/smartfridge/
+sudo mkdir /var/smartfridge/
+
+pip3 install opencv-python
+pip3 install --upgrade testresources
+pip3 install --upgrade setuptools
+pip3 install --upgrade google-api-python-client
+pip3 install google-auth-oauthlib
