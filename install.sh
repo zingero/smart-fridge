77c87d2900bc8f69521008e08f20679f4ff8eab3
Orian Zinger <zinger777@gmail.com>
2020-04-13 14:13:03 +0300

    Open-cv installer for rpi added

--- install.sh
+++ install.sh
@@ -1,2 +1,34 @@
-mkdir /etc/smartfridge/
-mkdir /var/smartfridge/
+sudo mkdir /etc/smartfridge/
+sudo mkdir /var/smartfridge/
+
+# install opencv taken from https://www.pyimagesearch.com/2019/09/16/install-opencv-4-on-raspberry-pi-4-and-raspbian-buster
+sudo apt-get purge wolfram-engine
+sudo apt-get purge libreoffice*
+sudo apt-get clean
+sudo apt-get autoremove
+
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install build-essential cmake pkg-config
+sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install libxvidcore-dev libx264-dev
+sudo apt-get install libfontconfig1-dev libcairo2-dev
+sudo apt-get install libgdk-pixbuf2.0-dev libpango1.0-dev
+sudo apt-get install libgtk2.0-dev libgtk-3-dev
+sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get install libhdf5-dev libhdf5-serial-dev libhdf5-103
+sudo apt-get install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
+sudo apt-get install python3-dev
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo python3 get-pip.py
+sudo rm -rf ~/.cache/pip
+sudo pip install virtualenv virtualenvwrapper
+echo "# virtualenv and virtualenvwrapper" >> ~/.bashrc
+echo "export WORKON_HOME=\$HOME/.virtualenvs" >> ~/.bashrc
+echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.bashrc
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+source ~/.bashrc
+mkvirtualenv cv -p python3
+pip install "picamera[array]"
+pip install opencv-contrib-python==4.1.0.25
