a308a5aceee112479d57a18c4678d3b8112c91b5
Orian Zinger <zinger777@gmail.com>
2020-04-14 14:42:55 +0300

    Installing google library in rpi

--- desktopinstaller.sh
+++ desktopinstaller.sh
@@ -2,6 +2,8 @@ sudo mkdir /etc/smartfridge/
 sudo mkdir /var/smartfridge/
 
 pip3 install opencv-python
+
+# install google api
 pip3 install --upgrade testresources
 pip3 install --upgrade setuptools
 pip3 install --upgrade google-api-python-client
--- rpiinstaller.sh
+++ rpiinstaller.sh
@@ -1,24 +1,26 @@
 sudo mkdir /etc/smartfridge/
+sudo chown pi:pi /etc/smartfridge/
 sudo mkdir /var/smartfridge/
+sudo chown pi:pi /var/smartfridge/
 
 # install opencv taken from https://www.pyimagesearch.com/2019/09/16/install-opencv-4-on-raspberry-pi-4-and-raspbian-buster
-sudo apt-get purge wolfram-engine
-sudo apt-get purge libreoffice*
-sudo apt-get clean
-sudo apt-get autoremove
+sudo apt-get purge -y wolfram-engine
+sudo apt-get purge -y libreoffice*
+sudo apt-get clean -y
+sudo apt-get autoremove -y
 
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install build-essential cmake pkg-config
-sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt-get install libxvidcore-dev libx264-dev
-sudo apt-get install libfontconfig1-dev libcairo2-dev
-sudo apt-get install libgdk-pixbuf2.0-dev libpango1.0-dev
-sudo apt-get install libgtk2.0-dev libgtk-3-dev
-sudo apt-get install libatlas-base-dev gfortran
-sudo apt-get install libhdf5-dev libhdf5-serial-dev libhdf5-103
-sudo apt-get install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
-sudo apt-get install python3-dev
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install -y build-essential cmake pkg-config
+sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install -y libxvidcore-dev libx264-dev
+sudo apt-get install -y libfontconfig1-dev libcairo2-dev
+sudo apt-get install -y libgdk-pixbuf2.0-dev libpango1.0-dev
+sudo apt-get install -y libgtk2.0-dev libgtk-3-dev
+sudo apt-get install -y libatlas-base-dev gfortran
+sudo apt-get install -y libhdf5-dev libhdf5-serial-dev libhdf5-103
+sudo apt-get install -y libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
+sudo apt-get install -y python3-dev
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 sudo python3 get-pip.py
@@ -32,3 +34,9 @@ source ~/.bashrc
 mkvirtualenv cv -p python3
 pip install "picamera[array]"
 pip install opencv-contrib-python==4.1.0.25
+
+# install google api
+pip3 install --upgrade testresources
+pip3 install --upgrade setuptools
+pip3 install --upgrade google-api-python-client
+pip3 install google-auth-oauthlib
